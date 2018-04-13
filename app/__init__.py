5e5978eba8917d3129140e56dba20ae25abbda8e
Aleksi Tanskanen <aleksi.tanskanen@helsinki.fi>
2018-04-13 23:17:27 +0300

    Heroku korjaus

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 import dj_database_url
 
 from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = dj_database_url.config(conn_max_age=500, require_ssl=True)
+app.config["SQLALCHEMY_DATABASE_URI"] = dj_database_url.config(conn_max_age=500)
 app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
