718228b737b12a64de09e6d6d5d90a3fa2532e8c
Aleksi Tanskanen <aleksi.tanskanen@helsinki.fi>
2018-04-13 23:30:54 +0300

    Heroku korjaus

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ import dj_database_url
 
 from flask_sqlalchemy import SQLAlchemy
 if os.environ['DATABASE_URL'] is none
-	app.config"SQLALCHEMY_DATABASE_URI"] = "postgres://acgzltixozxxpe:05a6cc8342b1a562eab32a1020d523a4b5ac50ded83fe1e1977741c4ecfccc2a@ec2-54-247-111-19.eu-west-1.compute.amazonaws.com:5432/ddqluf38h1mj3l"
+	app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://acgzltixozxxpe:05a6cc8342b1a562eab32a1020d523a4b5ac50ded83fe1e1977741c4ecfccc2a@ec2-54-247-111-19.eu-west-1.compute.amazonaws.com:5432/ddqluf38h1mj3l"
 else
 	app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
 app.config["SQLALCHEMY_ECHO"] = True
